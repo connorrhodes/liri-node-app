d3b8c70680f0fb8e46ef16be5c0b2c19242b7c5c
crhodes <connorrhodes1@yahoo.com>
2017-12-19 14:52:33 -0800

    changes

---
+++ keys.js
@@ -0,0 +1,12 @@
+//Twitter Consumer Key (API Key)	wvfH5AZL7PtydSkSocKco3ejG
+
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'wvfH5AZL7PtydSkSocKco3ejG',
+  consumer_secret: 'n8mzmYFAWNeYipga5OAomWikQosD1nE7FzdmyPGALgTlQOUWd3',
+  access_token_key: '943244410984075265-gUeepaaXeiNCchMFvZuBgAqDHs4CgSX',
+  access_token_secret: 'jqWjHGH3gbOqDS4ilnARr2Eerl6J4xE1NAt8zXoNuQx8Y',
+}
+
+module.exports = twitterKeys;
